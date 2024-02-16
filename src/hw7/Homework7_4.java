0bb2bcf62f3c102a8906266bb7468f6776703f2e
Hank <hank861202@gmail.com>
2024-02-16 15:05:36 +0800

    modify hw7

--- src/hw7/Homework7_4.java
+++ src/hw7/Homework7_4.java
@@ -15,46 +15,6 @@ public class Homework7_4 {
 //	承上題,請寫一個程式,能讀取Object.ser這四個物件,並執行speak()方法觀察結果如何 (請利用多
 //	型簡化本題的程式設計)
 
-//	public static void main(String[] args) throws Exception {
-//		File dir = new File("C:\\data");
-//		if (!dir.exists()) {
-//			dir.mkdir();
-//		}
-//
-//		File file = new File("C:\\data\\Object.ser");
-//		Dog[] dogs = new Dog[2];
-//		dogs[0] = new Dog("dog1");
-//		dogs[1] = new Dog("dog2");
-//		Cat[] cats = new Cat[2];
-//		cats[0] = new Cat("cat1");
-//		cats[1] = new Cat("cat2");
-//
-//		FileOutputStream fos = new FileOutputStream(file);
-//		ObjectOutputStream oos = new ObjectOutputStream(fos);
-//		for (int i = 0; i < dogs.length; i++) {
-//			oos.writeObject(dogs[i]);
-//			oos.writeObject(cats[i]);
-//		}
-//
-//		oos.close();
-//		fos.close();
-//
-//		FileInputStream fis = new FileInputStream(file);
-//		ObjectInputStream ois = new ObjectInputStream(fis);
-//		try {
-//			while (true) {
-//				((Dog) ois.readObject()).speak();
-//				System.out.println("--------------------");
-//				((Cat) ois.readObject()).speak();
-//				System.out.println("--------------------");
-//			}
-//		} catch (EOFException e) {
-//			System.out.println("資料讀取完畢！");
-//		}
-//
-//		ois.close();
-//		fis.close();
-
 	public static void main(String[] args) throws Exception {
 		File dir = new File("C:\\data");
 		if (!dir.exists()) {
